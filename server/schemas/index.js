1e400b6a4745a119d95bdc4ec61c5d54c1e017d1
FavioA <134189396+FavioA@users.noreply.github.com>
2023-11-27 00:29:50 -0800

    Added resolvers and typeDefs files with logic.

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,3 +1,4 @@
+// Import GraphQL schema and resolvers from separate files and exporting them as a single object.
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
